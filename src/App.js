2122fcf20f6dfc42208cef82ec6433eb895defcd
ebwinters@comcast.net <ebwinters@comcast.net>
2020-07-18 16:22:44 -0500

    begin homepage layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BlockPicker } from 'react-color';
+import Select from 'react-select'
 import './App.css';
+
+const ColorPicker = () => {
+  const [color, setColor] = useState('#ffffff');
+  return (
+    <BlockPicker 
+      triangle = "hide"
+      color = {color}
+      onChangeComplete={ (color) => setColor(color) }
+    />
+  );
+}
+
+const CodePicker = () => {
+  return (
+    <select>
+      <option value="grapefruit">Grapefruit</option>
+      <option value="lime">Lime</option>
+      <option selected value="coconut">Coconut</option>
+      <option value="mango">Mango</option>
+    </select>
+  );
+}
+
+const InstructionStep = (props) => {
+  return (
+    <div className="step">
+      <div className="step-header">
+        <div className="circle">
+          {`${props.number}`}
+        </div>
+        <p>{props.titleText}</p>
+      </div>
+      {props.children}    
+    </div>
+
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-body">
+        <InstructionStep number={1} titleText="Choose a background color">
+          <ColorPicker />
+        </InstructionStep>
+        <InstructionStep number={2} titleText="Select a language">
+          <CodePicker />
+        </InstructionStep>
+        <InstructionStep number={1} titleText="Choose a background color">
+          <ColorPicker />
+        </InstructionStep>
+      </div>
     </div>
   );
 }
